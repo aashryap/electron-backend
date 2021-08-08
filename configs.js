b0fca5a38d510fc43ec1581056f07e26be355f1c
Aashrya Pandya <aashryapandya@Aashryas-MacBook-Pro.local>
2021-08-08 21:21:43 +0530

    init

---
+++ configs.js
@@ -0,0 +1,7 @@
+module.exports = {
+    twilio: {
+        accountSid: 'AC65278ae44c13131bd87cc7e4ea6d4810',
+        apiKey: 'SK555e7fb53862acdb8faf7d6f6298a1b1',
+        apiSecret: 'rn24Wv9LuZlsGoj1UPGVXNVLASYmL4qE'
+    }
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const configs = require("./configs");
+const {videoToken} = require('./token');
+const app = express();
+const PORT = 4000;
+
+app.use(cors("*"));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Video server");
+})
+
+app.post('/video/token', (req, res) => {
+    try {
+        console.log("REQUEST ", req.body);
+        const identity = req.body.identity;
+        const room = req.body.room;
+        const token = videoToken(identity, room, configs);
+        console.log("TOKEN ", token);
+        res.status(200).send({token: token.toJwt()});
+    } catch(err) {
+        res.status(500).send("Internal server error");
+    }
+})
+
+app.listen(PORT, () => {
+    console.log("Server started on port ", PORT);
+});
